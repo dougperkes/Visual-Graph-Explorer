fd4e92a8099ba1d1c7a5455a01e25b73c3d36c54
Doug Perkes <dougperkes@msn.com>
2015-12-15 10:37:44 -0700

    Refactored to improve config for dev vs production.  Still more work to go.

--- App/Scripts/app.js
+++ App/Scripts/app.js
@@ -1,7 +1,25 @@
 'use strict';
 
-var app = angular.module('graphApp', ['ngRoute', 'AdalAngular', 'angularMoment'])
-    .config(['$routeProvider', '$httpProvider', 'adalAuthenticationServiceProvider', function ($routeProvider, $httpProvider, adalProvider) {
+var app = angular.module('graphApp', ['ngRoute', 'AdalAngular', 'angularMoment']);
+
+var PRODUCTION_URL = "visualgraphexplorer\.azurewebsites\.net";
+var clientId = null;
+var azureADConfig = {};
+azureADConfig.endpoints = {
+            // Map the location of a request to an API to a the identifier of the associated resource
+            'https://graph.microsoft.com': 'https://graph.microsoft.com'
+        };
+        
+if (RegExp(PRODUCTION_URL, 'i').test(window.location.host)) {
+    azureADConfig.clientId = '78d5a9a4-c418-4199-ac9b-a7e66a61ad91';
+} else {
+    azureADConfig.clientId = '223b5936-e31e-4036-a813-af8e976e7e87';
+}
+
+app.constant('azureADConfig', azureADConfig);
+
+app
+    .config(['$routeProvider', '$httpProvider', 'adalAuthenticationServiceProvider', 'azureADConfig', function ($routeProvider, $httpProvider, adalProvider, azureADConfig) {
 
         $routeProvider.when("/Home", {
             controller: "homeCtrl",
@@ -19,35 +37,16 @@ var app = angular.module('graphApp', ['ngRoute', 'AdalAngular', 'angularMoment']
             templateUrl: "/App/Views/D3.html",
         }).otherwise({ redirectTo: "/Home" });
 
-        var endpoints = {
-            // Map the location of a request to an API to a the identifier of the associated resource
-            'https://graph.microsoft.com': 'https://graph.microsoft.com'
-        };
-
-        var PRODUCTION_URL = "visualgraphexplorer\.azurewebsites\.net";
-        if (RegExp(PRODUCTION_URL, 'i').test(window.location.host)) {
-            adalProvider.init(
-                {
-                    instance: 'https://login.microsoftonline.com/',
-                    //tenant: 'perkesdemo.onmicrosoft.com',
-                    clientId: '78d5a9a4-c418-4199-ac9b-a7e66a61ad91',
-                    extraQueryParameter: 'nux=1',
-                    endpoints: endpoints,
-                },
-                $httpProvider
-                );
-        } else { //this is development
-            adalProvider.init(
-                {
-                    instance: 'https://login.microsoftonline.com/',
-                    //tenant: 'perkesdemo.onmicrosoft.com',
-                    clientId: '223b5936-e31e-4036-a813-af8e976e7e87',
-                    extraQueryParameter: 'nux=1',
-                    endpoints: endpoints,
-                },
-                $httpProvider
-                );
-        }
+        adalProvider.init(
+            {
+                instance: 'https://login.microsoftonline.com/',
+                //tenant: 'perkesdemo.onmicrosoft.com',
+                clientId: azureADConfig.clientId,
+                extraQueryParameter: 'nux=1',
+                endpoints: azureADConfig.endpoints,
+            },
+            $httpProvider
+            );
     }]);
 
 app.factory('graphHttpInterceptor', ['$q', '$rootScope', '$injector',
